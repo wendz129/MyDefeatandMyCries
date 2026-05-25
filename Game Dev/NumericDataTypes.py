9e9e11f3392f259988cd06df563245a6107c897d
We N D eLL <159757637+wendz129@users.noreply.github.com>
2026-05-25 23:52:19 +0800

    Update NumericDataTypes.py

--- Game Dev/NumericDataTypes.py
+++ Game Dev/NumericDataTypes.py
@@ -1,6 +1,14 @@
-Numeric Data Types
-Numeric data types are used to store numeric values. It can be an integer, floating number or even a complex number. Python supports three main numeric types:
+# Numeric Data Types
+# Numeric data types are used to store numeric values. It can be an integer, floating number or even a complex number. Python supports three main numeric types:
 
-Integers: value is represented by int class. It contains positive or negative whole numbers (without fractions or decimals).
-Float: value is represented by float class. It is a real number with a floating-point representation. It is specified by a decimal point.
-Complex: It is represented by a complex class. It stores numbers with real and imaginary parts. For example: 2+3j
+# Integers: value is represented by int class. It contains positive or negative whole numbers (without fractions or decimals).
+# Float: value is represented by float class. It is a real number with a floating-point representation. It is specified by a decimal point.
+# Complex: It is represented by a complex class. It stores numbers with real and imaginary parts. For example: 2+3j
+
+a = 5
+b = 5.0
+c = 2 + 4j
+
+print(a)
+print(b)
+print(c)
